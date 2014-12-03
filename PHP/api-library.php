9f74f38512b587acf9e92ac6a4a692cc2a6b0384
BrainCert <yasin@braincert.com>
2014-12-03 16:09:40 -0500

    Create api-library.php

---
+++ PHP/api-library.php
@@ -0,0 +1,104 @@
+<?php
+class Braincert
+{
+	private $api_key;
+	private $apiendpoint = "https://api.braincert.com/v1";
+	 
+	public function __construct ($api_key)
+	{
+		$this->api_key	= $api_key;
+	}
+	
+	public function getclassdata($data=array())
+	{
+		$data['task'] = 'getclass';
+		return $this -> sendHttpRequest($data);
+	}
+
+	public function getclass($data=array())
+	{
+		$data['task'] = 'schedule';
+		return $this -> sendHttpRequest($data);
+	}
+	public function getClassList($data=array())
+	{
+		$data['task'] = 'listclass';
+		return $this -> sendHttpRequest($data);
+	}
+	public function getPrice($data=array())
+	{
+		$data['task'] = 'addSchemes';
+		return $this -> sendHttpRequest($data);
+	}
+	public function getDiscount($data=array())
+	{
+		$data['task'] = 'addSpecials';
+		return $this -> sendHttpRequest($data);
+	}
+	public function listdiscount($data=array())
+	{
+		$data['task'] = 'listdiscount';
+		return $this -> sendHttpRequest($data);
+	}
+	public function listPrices($data=array())
+	{
+		 
+		$data['task'] = 'listSchemes';
+		return $this -> sendHttpRequest($data);
+	}
+	 
+	public function getclassrecording($data=array())
+	{
+		$data['task'] = 'getclassrecording';
+		return $this -> sendHttpRequest($data);
+	}
+	public function removeclassrecording($data=array())
+	{
+		$data['task'] = 'removeclassrecording';
+		return $this -> sendHttpRequest($data);
+	}
+	public function changestatusrecording($data=array())
+	{
+		$data['task'] = 'changestatusrecording';
+		 
+		return $this -> sendHttpRequest($data);
+	}
+	public function removediscount($data=array())
+	{
+		$data['task'] = 'removediscount';
+		return $this -> sendHttpRequest($data);
+	}
+	public function removeprice($data=array())
+	{
+		$data['task'] = 'removeprice';
+		return $this -> sendHttpRequest($data);
+	}
+	public function removeclass($data=array())
+	{
+		$data['task'] = 'removeclass';
+		return $this -> sendHttpRequest($data);
+	}
+	public function getrecording($data=array())
+	{
+		$data['task'] = 'getrecording';
+		return $this -> sendHttpRequest($data);
+	}
+	
+	public function sendHttpRequest($data)
+	{
+		$data['apikey'] = $this -> api_key;
+	    $data_string = http_build_query($data);
+	 	$this -> apiendpoint = $this -> apiendpoint;//."/".$data['task'];
+		$ch = curl_init($this -> apiendpoint);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false); 
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		$result = curl_exec($ch);
+		
+
+		return $result;
+	}
+}
+?>
